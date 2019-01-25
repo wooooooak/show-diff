d0f77974487ab47cd61bfa7bf697d32b67f2f1d2
wooooooak <wooooooak@gmail.com>
2019-01-25 10:40:17 +0900

    LoadingDots 컴포넌트 리팩토링

--- src/component/LoadingDots/index.js
+++ src/component/LoadingDots/index.js
@@ -1,38 +1,12 @@
-import React, { Component } from "react";
-import styled, { keyframes } from "styled-components";
+import React from "react";
+import * as style from "./style";
 
-const BounceAnimation = keyframes`
-  0% { margin-bottom: 0; }
-  50% { margin-bottom: 15px }
-  100% { margin-bottom: 0; }
-`;
-
-const DotWrapper = styled.div`
-  display: flex;
-  align-items: flex-end;
-  height: 100px;
-`;
-
-const Dot = styled.div`
-  background-color: black;
-  border-radius: 50%;
-  width: 10px;
-  height: 10px;
-  margin: 0 5px;
-  animation: ${BounceAnimation} 0.5s linear infinite;
-  animation-delay: ${props => props.delay};
-`;
-
-class LoadingDots extends Component {
-  render() {
-    return (
-      <DotWrapper>
-        <Dot delay="0s" />
-        <Dot delay=".1s" />
-        <Dot delay=".2s" />
-      </DotWrapper>
-    );
-  }
-}
+const LoadingDots = () => (
+  <style.DotWrapper>
+    <style.Dot delay="0s" />
+    <style.Dot delay=".1s" />
+    <style.Dot delay=".2s" />
+  </style.DotWrapper>
+);
 
 export default LoadingDots;
